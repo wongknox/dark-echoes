782e52f2ea17183523413d6b11355dce6edd9451
Knox Wong <wongknox@gmail.com>
2025-04-29 23:59:34 -0500

    Update App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,56 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+import "./index.css";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  function episodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>
+          Episode&nbsp;{selectedEpisode.id}:&nbsp;
+          {selectedEpisode.title}
+        </h2>
+        <p>{selectedEpisode.description}</p>
+      </section>
+    );
+  }
+
+  function episodeGuide() {
+    return (
+      <section className="episodeGuide">
+        <h2>Episodes</h2>
+        <ul className="episodeGuide">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main className="main-content">
+        {episodeGuide()}
+        {episodeDetails()}
+      </main>
+    </>
+  );
 }
